d83638b006e8efa0a2ff5c10c03a1555c56dd24d
Juan Carlos Manzanero Domínguez <whenix.developer@gmail.com>
2022-03-29 21:46:37 -0600

    add task dialog and functionality

--- src/app/kanban/dialogs/task-dialog/task-dialog.component.ts
+++ src/app/kanban/dialogs/task-dialog/task-dialog.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { BoardService } from '../../board.service';
 
 @Component({
-  selector: 'app-task-dialog',
-  templateUrl: './task-dialog.component.html',
-  styleUrls: ['./task-dialog.component.scss']
+    selector: 'app-task-dialog',
+    templateUrl: './task-dialog.component.html',
+    styleUrls: ['./task-dialog.component.scss']
 })
-export class TaskDialogComponent implements OnInit {
+export class TaskDialogComponent {
 
-  constructor() { }
+    public labelOptions = ['purple', 'blue', 'green', 'yellow', 'red', 'gray'];
 
-  ngOnInit(): void {
-  }
+    constructor(
+        public dialogRef: MatDialogRef<TaskDialogComponent>,
+        private ps: BoardService,
+        @Inject(MAT_DIALOG_DATA) public data: any
+    ) { }
+    
+    onNoClick(): void {
+        this.dialogRef.close();
+    }
 
+    handleTaskDelete() {
+        this.ps.removeTask(this.data.boardId, this.data.task);
+        this.dialogRef.close();
+    }
 }
